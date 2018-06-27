02dde0cc485e1f2d861a3d861db301e98b5f5b9d
Alkhafaji <alkhafml@miamioh.edu>
2018-06-27 18:51:50 -0400

    Allowing the cursor to be optional

--- app.js
+++ app.js
@@ -4,17 +4,14 @@ addEventListener("load", () => {
     function typeing(tag, speed) {
         //geting the information
         var words = tag.innerHTML;
-        var typing_speed = tag.dataset.typespeed === undefined? 0.2 : tag.dataset.typespeed;
-        var cursor_speed = tag.dataset.cursorspeed === undefined? 0.2 : tag.dataset.cursorspeed;
-        console.log(typing_speed)
+        var typing_speed = tag.dataset.typespeed === undefined ? 0.2 : tag.dataset.typespeed;
+        var cursor_speed = tag.dataset.cursorspeed === undefined ? 0.2 : tag.dataset.cursorspeed;
+        var cursor_wanted = tag.hasAttribute("data-cursor");
         //clear the tag
         tag.innerHTML = "";
         //creating span for the typing
         var typer_span = document.createElement("span");
         tag.appendChild(typer_span);
-        //create span for cursor 
-        var cursor_span = document.createElement("span");
-        tag.appendChild(cursor_span);
         //typing function
         var i = 0;
         var words_array = [];
@@ -26,14 +23,20 @@ addEventListener("load", () => {
                 clearInterval(cursor);
             } else i++;
         }, typing_speed * 1000)
-        //cursor function 
-        var cursor = setInterval(() => {
-            if (cursor_span.innerHTML === "") {
-                cursor_span.innerHTML = "|";
-            } else {
-                cursor_span.innerHTML = "";
-            }
-        }, cursor_speed*1000)
+        //Checking if the the user wants a cursor or not
+        if (cursor_wanted) {
+            //create span for cursor 
+            var cursor_span = document.createElement("span");
+            tag.appendChild(cursor_span);
+            //cursor function 
+            var cursor = setInterval(() => {
+                if (cursor_span.innerHTML === "") {
+                    cursor_span.innerHTML = "|";
+                } else {
+                    cursor_span.innerHTML = "";
+                }
+            }, cursor_speed * 1000)
+        }
     }
     typeing(tags);
 
