23d37d25d93191cf1bfd27a25e45753992701c1d
Alkhafaji <alkhafml@miamioh.edu>
2018-06-27 18:37:23 -0400

    adding cursor

--- app.js
+++ app.js
@@ -1,20 +1,39 @@
 "use strict";
-addEventListener("load",()=>{
+addEventListener("load", () => {
     var tags = document.querySelector("typer");
-    function typeing(tag,speed) {
+    function typeing(tag, speed) {
+        //geting the information
         var words = tag.innerHTML;
-        var speed = tag.dataset.speed;
-        var i=0;
+        var typing_speed = tag.dataset.typespeed;
+        var cursor_speed = tag.dataset.cursorspeed;
+        //clear the tag
+        tag.innerHTML = "";
+        //creating span for the typing
+        var typer_span = document.createElement("span");
+        tag.appendChild(typer_span);
+        //create span for cursor 
+        var cursor_span = document.createElement("span");
+        tag.appendChild(cursor_span);
+        //typing function
+        var i = 0;
         var words_array = [];
-        tags.innerHTML="";
-        var typer = setInterval(()=>{
+        var typer = setInterval(() => {
             words_array.push(words[i]);
-            tag.innerHTML = words_array.join("");
-            if(words.length === i) {
+            typer_span.innerHTML = words_array.join("");
+            if (words.length === i) {
                 clearInterval(typer);
-            } else i++;            
-        },speed*1000)
+                clearInterval(cursor);
+            } else i++;
+        }, typing_speed * 1000)
+        //cursor function 
+        var cursor = setInterval(() => {
+            if (cursor_span.innerHTML === "") {
+                cursor_span.innerHTML = "|";
+            } else {
+                cursor_span.innerHTML = "";
+            }
+        }, cursor_speed*1000)
     }
     typeing(tags);
-    
-    })
+
+})
