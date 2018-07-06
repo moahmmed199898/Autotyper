2268387e851661b2a4db39c310da290de083347d
Alkhafaji <alkhafml@miamioh.edu>
2018-07-06 16:37:38 -0400

    fixing some issues

--- app.js
+++ app.js
@@ -1,52 +1,64 @@
 "use strict";
 addEventListener("load", () => {
     var tags = document.querySelectorAll("typer");
-    //this will change in the next commit
-    var typing_same_time = true;
-    if(typing_same_time) {
-        for(var i = 0; i<tags.length; i++ ) {
-            typeing(tags[i]);
-        }
-    }
+    init(tags[0]);
+
+
 })
 
-function typeing(tag) {
+function init(tag) {
     //geting the information
     var words = tag.innerHTML;
     var typing_speed = tag.dataset.typespeed === undefined ? 0.2 : tag.dataset.typespeed;
     var cursor_speed = tag.dataset.cursorspeed === undefined ? 0.2 : tag.dataset.cursorspeed;
     var cursor_wanted = tag.hasAttribute("data-cursor");
+    prep(tag);
+    if (cursor_wanted) {
+        cursor(words, cursor_speed);
+    }
+    typing(words, typing_speed);
+}
+
+function prep(tag) {
     //clear the tag
     tag.innerHTML = "";
     //creating span for the typing
     var typer_span = document.createElement("span");
+    typer_span.className = "typer";
     tag.appendChild(typer_span);
-    //typing function
+    //create span for cursor 
+    var cursor_span = document.createElement("span");
+    cursor_span.className = "cursor"
+    tag.appendChild(cursor_span);
+}
+
+function typing(words, typing_speed) {
+    //look here there is a hot fix need to be changed!
+    let typer_span = document.getElementsByClassName("typer")[0];
     var i = 0;
     var words_array = [];
-    var typer = setInterval(() => {
+    let typer = setInterval(() => {
         words_array.push(words[i]);
         typer_span.innerHTML = words_array.join("");
         if (words.length === i) {
             clearInterval(typer);
-            clearInterval(cursor);
         } else i++;
-    }, typing_speed * 1000)
-    //Checking if the the user wants a cursor or not
-    if (cursor_wanted) {
-        //create span for cursor 
-        var cursor_span = document.createElement("span");
-        tag.appendChild(cursor_span);
-        //cursor function 
-        if(words.length % 2 !==0) {
-            cursor_span.innerHTML = "|";
-        }
-        var cursor = setInterval(() => {
-            if (cursor_span.innerHTML === "") {
-                cursor_span.innerHTML = "|";
-            } else {
-                cursor_span.innerHTML = "";
-            }
-        }, cursor_speed * 1000)
-    }
+    }, typing_speed * 1000);
 }
+
+function cursor(words, cursor_speed) {
+    //look here there is a hot fix need to be changed!
+    let cursor_span = document.getElementsByClassName("cursor")[0];
+    //cursor function 
+    if (words.length % 2 !== 0) {
+        cursor_span.innerHTML = "|";
+    }
+    var cursor = setInterval(() => {
+        if (cursor_span.innerHTML === "") {
+            cursor_span.innerHTML = "|";
+        } else {
+            cursor_span.innerHTML = "";
+        }
+    }, cursor_speed * 1000)
+}
+
