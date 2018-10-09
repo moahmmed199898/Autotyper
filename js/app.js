2b1dc4b571f1491ec060a5dec6d8fcd6aab6cf9b
Alkhafaji <alkhafml@middletowntechnologyservicess-iMac.local>
2018-10-09 10:17:23 -0400

    adding pause/unpause and typing speed feature

--- js/app.js
+++ js/app.js
@@ -1,17 +1,28 @@
-(() => {
     "use strict";
 
     class Typer {
         constructor(tag) {
             this.tag = tag;
             this.words = this.tag.innerHTML.split("");
+            //making delaystart optional
             this.delaystart = this.tag.dataset.delaystart || null
-            this._i = 0;
+            //set up the speed of typing in secounds
+            // the defult is 300ms
+            this.speed = this.tag.dataset.speed || 0.3
+            //this var stores the output array from 
+            // process to be rejoined in the render method
             this._output = [];
+            //this will determin weather the pause() method is called
+            // by default it's not paused
+            this._paused = false 
+            //this _i is used for the for loop in process() the reason it is in the constructor 
+            //is because I want to keep the progress when pausing and unpusing the code
+            this._i = 0
             this.init();
         }
-        //clear and decide whether time is required
+        
         init() {
+            //clear and decide whether time is required
             this.render();
             if(this.delaystart > 0){
                 this.timer();
@@ -19,19 +30,30 @@
                 this.process();
             }
         }
-        //delayes the typing for this.delaystart
+        
         timer() {
+            //delayes the typing for this.delaystart
             this.render()
             setTimeout(() => {
                 this.process();
             }, this.delaystart*1000)
         }
 
-        //core function
-        process() {
+        pause() {
+            this._paused = true
+        }
+        unpause() {
+            this._paused = false
+            this.process()
+        }
 
-            //This method is the core method of the module. This will run throw every letter and store in an array inside this._output then it will call render on it. 
+        process() {
+            //core function
+            // This method is the core method of the module.
+            // This will run throw every letter and store in an array inside
+            // this._output then it will call render on it. 
             let _interval = setInterval(() => {
+                this._paused ? clearInterval(_interval) : null
                 this._output.push(this.words[this._i]);
                 this.render();
                 if (this._i === this.words.length) {
@@ -39,21 +61,22 @@
                 } else {
                     this._i++;
                 }
-            }, 300);
+            }, this.speed*1000);
 
         }
-        //simply output whatever in this._output
         render() {
+            //simply output whatever in this._output
             this.tag.innerHTML = this._output.join("");
         }
     }
 
   //This code is used to automate the code so as soon as the page loads it will loop throw all the elements that have the tag "typer" 
   //and run new Typer on it. Each tag can be customized using the data- attributes
-  window.addEventListener("load",()=>{
-        let tags = document.getElementsByTagName("typer");
-        for(let i = 0; i<tags.length; i++) {
-            new Typer(tags[i])
-        }
-    })
-})()
+//   window.addEventListener("load",()=>{
+//         let tags = document.getElementsByTagName("typer");
+//         for(let i = 0; i<tags.length; i++) {
+//             let a = new Typer(tags[i])
+//         }
+//     })
+
+    var a = new Typer(document.getElementsByTagName("typer")[1])
